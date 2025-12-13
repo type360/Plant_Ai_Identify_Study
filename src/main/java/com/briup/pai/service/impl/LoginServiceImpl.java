2d5d7b55fa98ef8fdaa3588aacac5bf2a500e3cc
if <2492004509@qq.com>
2025-12-13 16:52:50 +0800

    完成利用账号密码登录模块（JWT）

--- src/main/java/com/briup/pai/service/impl/LoginServiceImpl.java
+++ src/main/java/com/briup/pai/service/impl/LoginServiceImpl.java
@@ -1,17 +1,41 @@
 package com.briup.pai.service.impl;
 
+import com.briup.pai.common.constant.LoginConstant;
+import com.briup.pai.common.enums.ResultCodeEnum;
+import com.briup.pai.common.enums.UserStatusEnum;
+import com.briup.pai.common.exception.BriupAssert;
+import com.briup.pai.common.utils.JwtUtil;
 import com.briup.pai.entity.dto.LoginWithPhoneDTO;
 import com.briup.pai.entity.dto.LoginWithUsernameDTO;
+import com.briup.pai.entity.po.User;
 import com.briup.pai.entity.vo.CurrentLoginUserVO;
 import com.briup.pai.service.ILoginService;
+import com.briup.pai.service.IUserService;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LoginServiceImpl implements ILoginService {
+    @Autowired
+    private IUserService userService;
 
     @Override
     public String loginWithUsername(LoginWithUsernameDTO dto) {
-        return "";
+        //这里要使用LoginServiceImpl->UserServiceImpl->UserMapper->User
+        //对比用户名称判断用户名称是否存在
+        User user = BriupAssert.requireNotNull(userService, User::getUsername, dto.getUsername(), ResultCodeEnum.USER_NOT_EXIST);
+        //对比密码判断密码是否正确
+        BriupAssert.requireEqual(DigestUtils.md5Hex(dto.getPassword()),user.getPassword(),ResultCodeEnum.PASSWORD_IS_WRONG);
+        //对比用户状态吗，判断用户状态
+        BriupAssert.requireEqual(user.getStatus(), UserStatusEnum.AVAILABLE,ResultCodeEnum.USER_IS_DISABLED);
+        Map<String, Object> map = new HashMap<>();
+        map.put(LoginConstant.JWT_PAYLOAD_KEY, user.getId());
+        String token = JwtUtil.generateJwt(map);
+        return token;
     }
 
     @Override
