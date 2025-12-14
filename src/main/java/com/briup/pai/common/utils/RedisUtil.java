b053df79b60f48043d074a99bdd8538dfa1eb209
if <2492004509@qq.com>
2025-12-14 17:35:42 +0800

    v3认证模块 退出功能

---
+++ src/main/java/com/briup/pai/common/utils/RedisUtil.java
@@ -0,0 +1,42 @@
+package com.briup.pai.common.utils;
+
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 86151
+ * @program: Plant_Ai_Identify_Study
+ * @description
+ * @create 2025/12/14 15:21
+ **/
+
+@Component
+public class RedisUtil {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /* key相关操作 */
+    //短信验证码不能重复
+    public Boolean existKey(String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    /* string类型相关操作 */
+    // 设置string类型的值，带过期时间，单位：秒
+    public void set(String key, Object value, int expireTime) {
+        redisTemplate.opsForValue().set(key, value, expireTime, TimeUnit.SECONDS);
+    }
+
+    // 获取string类型的值
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
--- src/main/java/com/briup/pai/controller/LoginController.java
+++ src/main/java/com/briup/pai/controller/LoginController.java
@@ -57,6 +57,30 @@ public class LoginController {
         return Result.success(loginService.loginWithTelephone(dto));
     }
 
+    /*此处是前端处理：前端将令牌从请求头中移除了
+    * 为什么是前端处理而不是后端处理呢？
+    * 这里反应了JWT的弊端，即JWT一旦生成无法销毁，只能等他自动过期
+    *
+    * 通过后端接口请求，如果令牌时间很长，1个月
+    * 登录获取令牌后，记录下来 注意令牌不要太长时间
+    * 解决方案一：将令牌存到redis中，redis的key期限就是token期限
+    *           注销用户，将redis中的token删除
+    *           访问资源，比对redis中的令牌
+    *           通过这种方式解决jwt无法销毁问题
+    * 方案二：不用jwt,换别的token oatuh2框架支持内置token可以销毁[缺点是不能存id信息]
+    *       登录成功后，将信息存到threadlocal 要获取用户身份，从threadlocal直接取出来
+    *
+    * 将令牌放到请求头携带到后端
+    * 请求行
+    * 请求头
+    * 请求体
+    * 后端共享多次请求的资源 request,session,application
+    * 前端共享多次请求的资源 Cookie,sessionStorage,localStorage(全局的)
+    *
+    * 前端把令牌存在哪里？
+    * 第一次请求，产生令牌
+    * 第二次访问资源，携带【请求头】、【cookie】
+    * */
     @PostMapping("/logout")
     @Operation(summary = "退出登录")
     public Result logout() {
